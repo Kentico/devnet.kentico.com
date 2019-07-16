2d8d99cbb8b8d5da6794376cf0a37cc13f98fb9a
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-07-16 14:43:42 +0200

    add automatic check for json format

---
+++ scripts/checkExtensionsFormat.js
@@ -0,0 +1,60 @@
+const Joi = require('@hapi/joi');
+const extensions = require('../marketplace/extensions.json');
+
+const extensionSchema = Joi.object().keys({
+  name: Joi.string().max(40).required(),
+  description: Joi.string().max(160).required(),
+  thumbnailUrl: Joi.string().uri({ scheme: 'https' }).required(),
+  source: Joi.string().max(40).required(),
+  sourceUrl: Joi.string().uri({ scheme: 'https' }).required(),
+  version: Joi.string().regex(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/).required(),
+  kenticoVersion: Joi.string().regex(/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/).required(),
+  category: Joi.any().valid(
+    'module',
+    'webpart',
+    'website template',
+    'utility',
+    'mvc widget',
+    'mvc section',
+    'mvc form component',
+    'mvc inline editor',
+    'mvc personalization condition type',
+    'other')
+    .required(),
+  tags: Joi.array().items(Joi.string())
+});
+
+const extensionValid = (extension) => {
+  const result = Joi.validate(extension, extensionSchema);
+  if(result.error) {
+    console.error(result.error.message);
+    return false;
+  }
+  return true;
+};
+
+const extensionsValid = () => {
+  if (!Array.isArray(extensions)) {
+    console.error('Extensions are not an array');
+    return 1;
+  }
+
+  for (const extension of extensions) {
+    if(!extensionValid(extension)){
+      return false;
+    }
+  }
+
+  console.info('Extensions format is valid.')
+  return true;
+};
+
+if(extensionsValid()) {
+  return 0;
+} else {
+  return 1;
+}
+
+module.exports = {
+  extensionsValid,
+}
